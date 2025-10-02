09227c322d8fc8487f269d9a12770181329313b9
unknown <alghifari0244@gmail.com>
2025-10-02 16:57:50 +0700

    first commit

---
+++ PRAK 3/MAHASISWA.java
@@ -0,0 +1,21 @@
+class MAHASISWA {
+    String NPM;
+    String namaDepan;
+    String namaBelakang;
+
+    MAHASISWA(String NPM, String namaDepan, String namaBelakang) {
+        this.NPM = NPM;
+        this.namaDepan = namaDepan;
+        this.namaBelakang = namaBelakang;
+    }
+    void belajar() {
+    System.out.println(this.NPM + " " + this.namaDepan + " " + this.namaBelakang + " " + "sedang belajar.");
+}
+void ujian() {
+    System.out.println(this.NPM + " " + this.namaDepan + " " + this.namaBelakang + " " + "sedang belajar.");
+}
+
+public static void main(String[] args) {
+    MAHASISWA David = 
+}
+}
